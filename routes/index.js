15a8aea64cd67ba42dd7e1e969d3a1e18903fd1e
udit <raiudit80@gmail.com>
2022-02-20 23:50:02 -0800

    api requesting and directory setup

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,6 @@ router.get('/',homeController.home);
 router.use('/users',require('./users'));
 router.use('/posts',require('./post'));
 router.use('/comments',require('./comments'));
+router.use('/api',require('./api'));
 module.exports=router; 
+ 
